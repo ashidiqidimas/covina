9a0d524f990273152bd693e17b2060038350a5d9
ashidiqidimas <prabowo6@gmail.com>
2021-07-22 17:36:17 +0700

    Initial Commit

---
+++ Covina/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covina
+//
+//  Created by Dimas on 22/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
